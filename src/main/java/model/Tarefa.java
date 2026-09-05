b6eb9ec4f5ebfba6c8c2bb8435dedd46c011b97e
Giovani Boldrini Custoias <gcustoias@gmail.com>
2026-09-05 09:07:23 -0300

    feat: cria entidade Tarefa

---
+++ src/main/java/model/Tarefa.java
@@ -0,0 +1,49 @@
+package com.giovani.desafiotecnico.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tarefas")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String descricao;
+
+    public Tarefa() {
+    }
+
+    public Tarefa(Long id, String titulo, String descricao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
